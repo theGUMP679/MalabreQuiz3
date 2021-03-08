0545d6509c022a02c9cd5af765f157245697a6e1
theGUMP679 <78369496+theGUMP679@users.noreply.github.com>
2021-03-08 15:52:22 -0500

    Lab3

---
+++ Assets/Scripts/Bear.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bear : Items
+{
+    public Text playerMessage;
+    public Text Stats;
+    private int Intake;
+    private int Karma;
+
+    private void Awake()
+    {
+        Intake = 0;
+        Karma = 0;
+    }
+    public void WriteMessage()
+    {
+        playerMessage.text = "You hurts me";
+    }
+
+    public void UpdateStats(int cals, int karm)
+    {
+        Intake += Calories;
+        Karma += Karmas;
+        Stats.text = "Your caloric intake is " + Intake + " and your karma is " + Karma;
+        if ((Intake < -30) || (Karma < -20))
+        {
+            Stats.text = "You Died!";
+        }
+
+
+    }
+
+    public override void UseItem()
+    {
+        base.UseItem();
+        UpdateStats(Karma, Intake);
+        WriteMessage();
+        Debug.Log("Put things here");
+
+
+    }
+}
---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+
+	public string Name;
+	public int Calories;
+	public int Karmas;
+	public bool Alive;
+	public string Message;
+
+	private void OnMouseDown()
+	{
+		UseItem();
+	}
+
+	public virtual void UseItem()
+
+	{
+		//two lines below find a script called TellPlayer and call functions in it
+		//FindObjectOfType<TellPlayer>().WriteMessage(Message);
+		//FindObjectOfType<TellPlayer>().UpdateStats(Calories, Karmas);
+
+	}
+}
